bb12f76ad1dde7a27428cda23240081588ad92ab
felipesousa <felipesousa@users.noreply.github.com>
2020-03-21 03:46:16 +0000

    deploy: f852d33ad6d2efb4fae4d09a7eeec6c82503b6ca

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"1":"Module","64":"Function"},"rows":[{"id":0,"kind":1,"name":"\"index\"","url":"modules/_index_.html","classes":"tsd-kind-module"},{"id":1,"kind":64,"name":"get","url":"modules/_index_.html#get","classes":"tsd-kind-function tsd-parent-kind-module","parent":"\"index\""}]};
